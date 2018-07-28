56ead52161d8a3895c60cdd660a54851251e9c0c
FEWY <937415540@qq.com>
2018-07-28 23:08:40 +0800

    增加图片

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -36,6 +36,13 @@ Page({
       '../../images/1.jpg',
       '../../images/2.jpg',
       '../../images/3.jpg',
+      '../../images/4.jpg',
+      '../../images/5.jpg',
+      '../../images/6.jpg',
+      '../../images/7.jpg',
+      '../../images/8.jpg',
+      '../../images/9.jpg',
+      '../../images/10.jpg',
     ]
   },
 
